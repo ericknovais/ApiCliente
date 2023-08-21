f92e375a0edcdebe67fe2c0e1ca2f0626f9a8bd8
ericknovais <erick.hora@outlook.com>
2023-08-21 20:53:25 -0300

    Criação da API Cliente

---
+++ ApiCliente/ApiCliente.API/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using ApiCliente.DataAccess.repositorio;
+using ApiCliente.DataModel.modelo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCliente.API.Controllers
+{
+    //[ApiController]
+    //[Route("[controller]")]
+    public class ClienteController
+    {
+        Repositorio repositorio = new Repositorio();
+        [HttpGet("GetClientes")]
+        public List<Cliente> Cliente() 
+        {
+            try
+            {
+                var lista = repositorio.Cliente.ObterTodos();
+                return lista.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            } 
+        }
+    }
+}
